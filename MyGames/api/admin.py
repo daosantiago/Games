acc08fff290cc2e470ae6a7668dd37d1db484325
Anderson <daosantiago@gmail.com>
2023-08-11 14:29:49 +0100

    create ModelAdmin for Game

--- MyGames/api/admin.py
+++ MyGames/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Game
+
 # Register your models here.
+
+
+class Games(admin.ModelAdmin):
+  list_display = ('name', 'description')
+  class Meta:
+    model=Game
+    admin.site.register([Game])
+    
