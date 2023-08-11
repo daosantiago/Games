652b508ee277ab0798ebb69289218fadaf270d3c
Anderson <daosantiago@gmail.com>
2023-08-11 16:59:21 +0100

    Alter field name to title

--- MyGames/api/models.py
+++ MyGames/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Item(models.Model):
+    """Modelo para representar as informações comuns para todas as tabelas do banco de dados"""
     description = models.TextField("Descrição")
     created = models.DateTimeField('Criado em ', auto_now_add=True)
     updated = models.DateTimeField('Atualizado em ', auto_now=True)
@@ -12,4 +13,13 @@ class Item(models.Model):
 
 
 class Game(Item):
-    name = models.CharField("<NAME>", max_length=1024, unique=True)
+    """Modelo para representar os jogos cadastrados"""
+    title = models.CharField("Nome do jogo", max_length=1024, unique=True)
+    # imagem = models.ImageField("Capa do Jogo ", upload_to="games")
+    
+    class Meta:
+        verbose_name='Jogo'
+        ordering=['title']
+
+    def __str__(self):
+        return self.titulo
