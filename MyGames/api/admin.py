eb208b2b6aa24bea8c4443886ff10088b405628c
Anderson <daosantiago@gmail.com>
2023-08-11 17:26:42 +0100

    register model console

--- MyGames/api/admin.py
+++ MyGames/api/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
 
-from .models import Game
+from .models import Game, Console
 
 # Register your models here.
 
 
 class Games(admin.ModelAdmin):
-  list_display = ('name', 'description')
+  list_display = ('id', 'title', 'description', 'created', 'updated')
+  list_display_links = ('id', 'title')
+  search_fields = ('title',)
+  list_per_page = 20
+
   class Meta:
     model=Game
-    admin.site.register([Game])
+
+admin.site.register(Game, Games)
+    
+class Consoles(admin.ModelAdmin):
+  list_display = ('id', 'name', 'description', 'created', 'updated')
+  list_display_links = ('id', 'name')
+  search_fields = ('name',)
+  list_per_page = 20
+  
+  class Meta:
+    model=Game
+
+admin.site.register(Console, Consoles)
     
