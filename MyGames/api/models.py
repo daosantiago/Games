95c3583f01f5355a4eb4d661409720243e6445ed
Anderson <daosantiago@gmail.com>
2023-08-11 11:48:39 +0100

    create base item

--- MyGames/api/models.py
+++ MyGames/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    description = models.TextField("Descrição")
+    created = models.DateTimeField('Criado em ', auto_created=True)
+    updated = models.DateTimeField('Atualizado em ', auto_now= True)
+    
