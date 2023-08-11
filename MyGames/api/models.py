d3753b6df7e141c59fc70508ce86f1a396f1afb6
Anderson <daosantiago@gmail.com>
2023-08-11 14:26:49 +0100

    edit Item model to be abstract

--- MyGames/api/models.py
+++ MyGames/api/models.py
@@ -4,6 +4,11 @@ from django.db import models
 
 class Item(models.Model):
     description = models.TextField("Descrição")
-    created = models.DateTimeField('Criado em ', auto_created=True)
-    updated = models.DateTimeField('Atualizado em ', auto_now= True)
-    
+    created = models.DateTimeField('Criado em ', auto_now_add=True)
+    updated = models.DateTimeField('Atualizado em ', auto_now=True)
+    active = models.BooleanField()
+
+    class Meta:
+        abstract = True
+
+
