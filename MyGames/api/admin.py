6fbbbe49f09b50e5135799f9ac264e02fe37ccf2
Anderson <daosantiago@gmail.com>
2023-08-15 16:05:31 +0100

    update

--- MyGames/api/admin.py
+++ MyGames/api/admin.py
@@ -1,29 +1,85 @@
 from django.contrib import admin
 
-from .models import Game, Console
+from .models import Game, Console, Genre, Difficulty, Evaluation
 
 # Register your models here.
 
 
 class Games(admin.ModelAdmin):
-  list_display = ('id', 'title', 'description', 'created', 'updated')
-  list_display_links = ('id', 'title')
-  search_fields = ('title',)
-  list_per_page = 20
+    list_display = (
+        "id",
+        "platform",
+        "title",
+        "genre",
+        "description",
+        "created",
+        "updated",
+    )
+    list_display_links = ("id", "title")
 
-  class Meta:
-    model=Game
+    fieldsets = (
+        (
+            "Informações do jogo",
+            {
+                "fields": ["title", "imagem", "genre", "platform"],
+            },
+        ),
+        ("Descrição", {"fields": ["description"]}),
+    )
+
+    search_fields = ("title",)
+    list_per_page = 20
+
+    class Meta:
+        model = Game
+
+
+class Consoles(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "created", "updated")
+    list_display_links = ("id", "name")
+
+    fields = ("name", "shortName", "description")
+    search_fields = ("name",)
+    list_per_page = 20
+
+    class Meta:
+        model = Console
+
+
+class Genres(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "created", "updated")
+    list_display_links = ("id", "name")
+
+    fields = ("name", "description")
+    search_fields = ("name",)
+    list_per_page = 20
+
+    class Meta:
+        model = Genre
+
+
+class Evaluations(admin.ModelAdmin):
+    list_display = ("id", "user", "title")
+    list_display_links = ("id", "user")
+    readonly_fields = ("user",)
+
+    fields = ("title", "description", "platform", "game", "user")
+
+    class Meta:
+        model = Evaluation
+
+    # def save_form(self, request, form, change):
+    #     obj = super(Evaluations, self).save_form(request, form, change)
+    #     if not change:
+    #         obj.user = request.user
+    #     return obj
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
 
 admin.site.register(Game, Games)
-    
-class Consoles(admin.ModelAdmin):
-  list_display = ('id', 'name', 'description', 'created', 'updated')
-  list_display_links = ('id', 'name')
-  search_fields = ('name',)
-  list_per_page = 20
-  
-  class Meta:
-    model=Game
-
 admin.site.register(Console, Consoles)
-    
+admin.site.register(Genre, Genres)
+admin.site.register(Evaluation, Evaluations)
--- MyGames/api/models.py
+++ MyGames/api/models.py
@@ -1,46 +1,115 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Item(models.Model):
     """Modelo para representar as informações comuns para todas as tabelas do banco de dados"""
+
     description = models.TextField("Descrição")
-    created = models.DateTimeField('Criado em ', auto_now_add=True)
-    updated = models.DateTimeField('Atualizado em ', auto_now=True)
+    created = models.DateTimeField("Criado em ", auto_now_add=True)
+    updated = models.DateTimeField("Atualizado em ", auto_now=True)
     active = models.BooleanField("Ativo", default=True)
 
     class Meta:
         abstract = True
 
 
-class Game(Item):
-    """Modelo para representar os jogos cadastrados"""
-    title = models.CharField("Nome do jogo", max_length=1024, unique=True)
-    imagem = models.ImageField("Capa do Jogo ", upload_to="games")
-    
-    class Meta:
-        verbose_name='Jogo'
-        ordering=['title']
-
-    def __str__(self):
-        return self.title
-
-    
-class Console(Item):
-    """Modelo para representar o consoles de video game"""
-    name = models.CharField("Nome do console",max_length=250)
-    shortName = models.CharField("Abreviação ou Apelido", max_length=50)
-
+class Genre(Item):
+    name = models.CharField("Gênero de jogo", max_length=50)
+    asd = models
 
     class Meta:
-        verbose_name='Console'
-        ordering=['name']
+        verbose_name = "Gênero"
+        verbose_name_plural = "Gêneros"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
-class Genre(Item):
-    name = models.CharField("Gênero de jogo",max_length=50)
+
+class Difficulty(Item):
+    name = models.CharField("Dificuldade do jogo", max_length=3)
 
     class Meta:
-        verbose_name='Gênero'
-        ordering=['name']
+        verbose_name = "Dificuldade"
+        verbose_name_plural = "Dificuldades"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Console(Item):
+    """Modelo para representar o consoles de video game"""
+
+    name = models.CharField("Nome do console", max_length=250)
+    shortName = models.CharField("Abreviação ou Apelido", max_length=50, blank=True)
+    generation = models.CharField(
+        "Geração do console", max_length=250, blank=True, null=True
+    )
+    color = models.CharField("Cor do console", blank=True, max_length=15)
+
+    class Meta:
+        verbose_name = "Console"
+        verbose_name_plural = "Consoles"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Game(Item):
+    """Modelo para representar os jogos cadastrados"""
+
+    title = models.CharField("Nome do jogo", max_length=1024, unique=True)
+    imagem = models.ImageField("Capa do Jogo ", upload_to="games", blank=True)
+    genre = models.ForeignKey(
+        Genre, verbose_name="Gênero", on_delete=models.CASCADE, blank=True, null=True
+    )
+    platform = models.ForeignKey(
+        Console,
+        verbose_name="Plataforma",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Jogo"
+        verbose_name_plural = "Jogos"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+
+class Evaluation(Item):
+    """Modelo para representar avaliação dos usuários"""
+
+    user = models.ForeignKey(User, verbose_name="Usuário", on_delete=models.CASCADE)
+    title = models.CharField(
+        "Título da avaliação", max_length=50, blank=True, null=True
+    )
+    platform = models.ForeignKey(
+        Console,
+        verbose_name="Plataforma",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    game = models.ForeignKey(
+        Game,
+        verbose_name="Jogo",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Avaliação"
+        verbose_name_plural = "Avaliações"
+        ordering = ["updated"]
+
+    def __str__(self):
+        return self.title
